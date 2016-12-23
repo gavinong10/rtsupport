795c2a7d01b73d7860b866ec4050e013717dc932
Gavin Ong <gavinong10@gmail.com>
2016-12-23 15:15:56 +1000

    Add users and messages area UI

--- components/App.jsx
+++ components/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
+import UserSection from './users/UserSection.jsx';
+import MessageSection from './messages/MessageSection.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      channels: []
+      channels: [],
+      users: [],
+      messages: [],
+      activeChannel: {},
+      connected: false
     };
   }
   addChannel(name) {
@@ -25,15 +31,49 @@ class App extends Component {
     });
   // TODO: Get Channels Messages
   }
+
+  setUserName(name) {
+    let {users} = this.state;
+    users.push({
+      id: users.length,
+      name
+    });
+    this.setState({
+      users
+    });
+  // TODO: Send to server
+  }
+  addMessage(body) {
+    let {messages, users} = this.state;
+    let createdAt = new Date;
+    let author = users.length > 0 ? users[0].name : 'anonymous'
+    messages.push({
+      id: messages.length,
+      body,
+      createdAt,
+      author
+    });
+    this.setState({
+      messages
+    });
+  // TODO: Send to server
+  }
+
   render() {
     return (
       <div className="app">
         <div className="nav">
+          <ChannelSection
+                          {...this.state}
+                          addChannel={ this.addChannel.bind(this) }
+                          setChannel={ this.setChannel.bind(this) } />
+          <UserSection
+                       {...this.state}
+                       setUserName={ this.setUserName.bind(this) } />
         </div>
-        <ChannelSection
+        <MessageSection
                         {...this.state}
-                        addChannel={ this.addChannel.bind(this) }
-                        setChannel={ this.setChannel.bind(this) } />
+                        addMessage={ this.addMessage.bind(this) } />
       </div>
     )
   }
